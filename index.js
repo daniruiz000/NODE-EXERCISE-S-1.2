70e28dc2869ec45d3d66e4b9a7b77536ada42fae
Daniel Ruiz <danielruimar@gmail.com>
2023-04-20 23:33:36 +0200

    finish exercise

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express();
 const router = express.Router();
 
 const homeFilePath = "./home.html";
-// const driversFilePath = "./driver-data.json";
+const driversFilePath = "./driver-data.json";
 
 //  Configuracion de Router para servidores que tengan rutas con parametros.PONER SIEMPRE QUE HAY PARAMS.
 server.use(express.json());
@@ -28,81 +28,81 @@ router.get("/", (req, res) => {
   });
 });
 
-// router.get("/pokemon", (req, res) => {
-//   fs.readFile(pokemonFilePath, (error, data) => {
-//     //  Leemos el fichero//
-//     if (error) {
-//       //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
-//       res.status(500).send("Error inesperado");
-//     } else {
-//       //  Si no hay error al leer el fichero ...
-//       const pokemons = JSON.parse(data);
-//       //  Parseamos el json
-//       res.json(pokemons);
-//       // Enviamos todos los pokemons en la respuesta
-//     }
-//   });
-// });
+router.get("/f1-driver", (req, res) => {
+  fs.readFile(driversFilePath, (error, data) => {
+    //  Leemos el fichero//
+    if (error) {
+      //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
+      res.status(500).send("Error inesperado");
+    } else {
+      //  Si no hay error al leer el fichero ...
+      const drivers = JSON.parse(data);
+      //  Parseamos el json
+      res.json(drivers);
+      // Enviamos todos los drivers en la respuesta
+    }
+  });
+});
 
-// router.post("/pokemon", (req, res) => {
-//   fs.readFile(pokemonFilePath, (error, data) => {
-//     //  Leemos el fichero//
-//     if (error) {
-//       //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
-//       res.status(500).send("Error inesperado");
-//     } else {
-//       //  Si no hay error al leer el fichero ...
-//       const pokemons = JSON.parse(data);
-//       //  Parseamos el json
-//       const newPokemon = req.body;
-//       //  Recogemos la peticion post en el body de la request
-//       const lastId = pokemons[pokemons.length - 1].id;
-//       //  Identificamos el último id del fichero
-//       newPokemon.id = lastId + 1;
-//       //  Asignamos un id al nuevo pokemon que vamos a añadir
-//       pokemons.push(newPokemon);
-//       //  Lo añadimos a la lista de pokemons
+router.post("/f1-driver", (req, res) => {
+  fs.readFile(driversFilePath, (error, data) => {
+    //  Leemos el fichero//
+    if (error) {
+      //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
+      res.status(500).send("Error inesperado");
+    } else {
+      //  Si no hay error al leer el fichero ...
+      const drivers = JSON.parse(data);
+      //  Parseamos el json
+      const newDriver = req.body;
+      //  Recogemos la peticion post en el body de la request
+      const lastId = drivers[drivers.length - 1].id;
+      //  Identificamos el último id del fichero
+      newDriver.id = lastId + 1;
+      //  Asignamos un id al nuevo driver que vamos a añadir
+      drivers.push(newDriver);
+      //  Lo añadimos a la lista de drivers
 
-//       // Guardamos el fichero
-//       fs.writeFile(pokemonFilePath, JSON.stringify(pokemons), (error) => {
-//         // Ecribimos el fichero con los pokemons nuevos
-//         if (error) {
-//           //   Si hay error ... le envio un código 500 y un mensaje
-//           res.status(500).send("Error al escribir el archivo");
-//         } else {
-//           //  Si no hay error y se ha ecrito correctamente el archivo le devuelvo el pokemon nuevo
-//           res.json(newPokemon);
-//         }
-//       });
-//     }
-//   });
-// });
+      // Guardamos el fichero
+      fs.writeFile(driversFilePath, JSON.stringify(drivers), (error) => {
+        // Ecribimos el fichero con los drivers nuevos
+        if (error) {
+          //   Si hay error ... le envio un código 500 y un mensaje
+          res.status(500).send("Error al escribir el archivo");
+        } else {
+          //  Si no hay error y se ha ecrito correctamente el archivo le devuelvo el pokemon nuevo
+          res.json(newDriver);
+        }
+      });
+    }
+  });
+});
 
-// router.get("/pokemon/:id", (req, res) => {
-//   fs.readFile(pokemonFilePath, (error, data) => {
-//     //  Leemos el fichero//
-//     if (error) {
-//       //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
-//       res.status(500).send("Error inesperado");
-//     } else {
-//       //  Si no hay error al leer el fichero ...
-//       const id = parseInt(req.params.id);
-//       //  Convertimos a numerico el id indicado por parametros
-//       const pokemons = JSON.parse(data);
-//       //  Parseamos el json
-//       const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-//       //  Buscamos el pokemon con el mismo id q nos han indicado
+router.get("/f1-driver/:id", (req, res) => {
+  fs.readFile(driversFilePath, (error, data) => {
+    //  Leemos el fichero//
+    if (error) {
+      //  Si hay error al leer el fichero ... envio codigo 500 y un mensaje
+      res.status(500).send("Error inesperado");
+    } else {
+      //  Si no hay error al leer el fichero ...
+      const id = parseInt(req.params.id);
+      //  Convertimos a numerico el id indicado por parametros
+      const drivers = JSON.parse(data);
+      //  Parseamos el json
+      const driver = drivers.find((driver) => driver.id === id);
+      //  Buscamos el pokemon con el mismo id q nos han indicado
 
-//       if (pokemon) {
-//         //  Si existe ese pokemon con ese id indicado...enviamos el pokemon en la respuesta
-//         res.json(pokemon);
-//       } else {
-//         //  Si no existe un pokemon con el id indicado ... enviamos un codigo 404 y un mensaje
-//         res.status(404).send("pokemon no encontrado");
-//       }
-//     }
-//   });
-// });
+      if (driver) {
+        //  Si existe ese pokemon con ese id indicado...enviamos el pokemon en la respuesta
+        res.json(driver);
+      } else {
+        //  Si no existe un pokemon con el id indicado ... enviamos un codigo 404 y un mensaje
+        res.status(404).send("Driver no encontrado");
+      }
+    }
+  });
+});
 
 server.use("/", router);
 
